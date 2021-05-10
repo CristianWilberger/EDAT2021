b67c1699f47e8f8b55324484487cb0365c51d1d7
Cristian Wilberger <Casa@DESKTOP-RAQU9T1>
2021-05-10 20:51:15 -0300

    Metodos parcial Arbol generico

--- EDAT2021/src/jerarquicas/ArbolGen.java
+++ EDAT2021/src/jerarquicas/ArbolGen.java
@@ -4,82 +4,134 @@
  * and open the template in the editor.
  */
 package jerarquicas;
+
 import lineales.dinamicas.Lista;
+import lineales.dinamicas.Cola;
+
 /**
  *
  * @author Usuario
  */
 public class ArbolGen {
-        
-        //Atributos
-        private NodoGen raiz;
-        
-        //Constructor
-        public ArbolGen(){
-            this.raiz=null;
+
+    //Atributos
+    private NodoGen raiz;
+
+    //Constructor
+    public ArbolGen() {
+        this.raiz = null;
+    }
+
+    //operaciones basicas
+    public boolean esVacio() {
+        return this.raiz == null;
+    }
+
+    //listados
+    public Lista listarPreorden() {
+        Lista salida = new Lista();
+        listarPreordenAux(this.raiz, salida);
+        return salida;
+    }
+
+    private void listarPreordenAux(NodoGen nodo, Lista lista) {
+        if (nodo != null) {
+            listarPreordenAux(nodo.getHermanoDerecho(), lista);
+            listarPreordenAux(nodo.getHijoIzquierdo(), lista);
+            lista.insertar(nodo.getElem(), 1);
         }
-        
-        
-        
-        
-        
-        
-        
-        //listados
-        public Lista listarInprden(){
-            Lista salida= new Lista();
-            listarInordenAux(this.raiz,salida);
-            return salida;
-        }
-        
-        
-        private void listarInordenAux(NodoGen n, Lista ls){
-            if (n !=null ) {
-                //LLamado recursivo con primer hijo de n
-                if (n.getHijoIzquierdo()!= null) {
-                    listarInordenAux(n.getHijoIzquierdo(),ls);
-                }
-                //visita del nodo n
-                ls.insertar(n.getElem(), ls.longitud()+1);
-                
-                //LLamados recursivos con los otros hijos de n
-                if (n.getHijoIzquierdo()!=null) {
-                    NodoGen hijo=n.getHijoIzquierdo().getHermanoDerecho();
-                    while(hijo!=null){
-                        listarInordenAux(hijo,ls);
-                        hijo=hijo.getHermanoDerecho();
-                    }
+    }
+
+    public Lista listarInorden() {
+        Lista salida = new Lista();
+        listarInordenAux(this.raiz, salida);
+        return salida;
+    }
+
+    private void listarInordenAux(NodoGen n, Lista ls) {
+        if (n != null) {
+            //LLamado recursivo con primer hijo de n
+            if (n.getHijoIzquierdo() != null) {
+                listarInordenAux(n.getHijoIzquierdo(), ls);
+            }
+            //visita del nodo n
+            ls.insertar(n.getElem(), ls.longitud() + 1);
+
+            //LLamados recursivos con los otros hijos de n
+            if (n.getHijoIzquierdo() != null) {
+                NodoGen hijo = n.getHijoIzquierdo().getHermanoDerecho();
+                while (hijo != null) {
+                    listarInordenAux(hijo, ls);
+                    hijo = hijo.getHermanoDerecho();
                 }
             }
         }
-        
-        
-        
-        
-        @Override
-        public String toString(){
-            return toStringAux(this.raiz);
+    }
+
+    public Lista listarPosorden() {
+        Lista lista = new Lista();
+        listarPosordenAux(this.raiz, lista);
+        return lista;
+    }
+
+    private void listarPosordenAux(NodoGen nodo, Lista lista) {
+        if (nodo != null) {
+            NodoGen hijo = nodo.getHijoIzquierdo();
+            while (hijo != null) {
+                listarPosordenAux(hijo, lista);
+                hijo = hijo.getHermanoDerecho();
+            }
+            lista.insertar(nodo.getElem(), lista.longitud() + 1);
         }
-        
-        private String toStringAux(NodoGen nodo){
-            String retorno="";
-            if (nodo!=null) {
-                //Visita del nodo 
-                retorno+=nodo.getElem().toString()+" -> ";
-                NodoGen hijo=nodo.getHijoIzquierdo();
-                while(hijo !=null){
-                    retorno+=hijo.getElem().toString() + ", ";
-                    hijo=hijo.getHermanoDerecho();
-                }
-                
-                //Comienza recorrido de los hijos de nodo , llamando recursivamente
-                //para que cada hijo agregue su subcadena a la general
-                hijo=nodo.getHijoIzquierdo();
-                while(hijo!=null){
-                    retorno+="\n"+ toStringAux(hijo);
-                    hijo=hijo.getHermanoDerecho();
+    }
+
+    public Lista listarNiveles() {
+        Lista lista = new Lista();
+        if (this.raiz != null) {
+            Cola q = new Cola();
+            q.poner(this.raiz);
+            while (!q.esVacia()) {
+                NodoGen nodo = (NodoGen) q.obtenerFrente();
+                q.sacar();
+                lista.insertar(nodo, lista.longitud() + 1);
+                NodoGen hijo = nodo.getHijoIzquierdo();
+                while (hijo != null) {
+                    q.poner(hijo);
+                    hijo = hijo.getHermanoDerecho();
                 }
             }
-            return retorno;
         }
+        return lista;
+    }
+
+    public void vaciar() {
+        this.raiz = null;
+    }
+
+    @Override
+    public String toString() {
+        return toStringAux(this.raiz);
+    }
+
+    private String toStringAux(NodoGen nodo) {
+        String retorno = "";
+        if (nodo != null) {
+            //Visita del nodo 
+            retorno += nodo.getElem().toString() + " -> ";
+            NodoGen hijo = nodo.getHijoIzquierdo();
+            while (hijo != null) {
+                retorno += hijo.getElem().toString() + ", ";
+                hijo = hijo.getHermanoDerecho();
+            }
+
+            //Comienza recorrido de los hijos de nodo , llamando recursivamente
+            //para que cada hijo agregue su subcadena a la general
+            hijo = nodo.getHijoIzquierdo();
+            while (hijo != null) {
+                retorno += "\n" + toStringAux(hijo);
+                hijo = hijo.getHermanoDerecho();
+            }
+        }
+        return retorno;
+    }
 }
